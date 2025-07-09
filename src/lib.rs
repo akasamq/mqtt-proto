e00853a56ab7daf86eecf3ce8abbeb6185d7dbae
Sieluna <seele.peng@gmail.com>
2025-07-09 14:01:46 +0300

    Move types under common

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,10 @@ mod common;
 pub mod v3;
 pub mod v5;
 
-pub(crate) use embedded_io::{Read as SyncRead, Write as SyncWrite};
-pub(crate) use embedded_io_async::{Read as AsyncRead, Write as AsyncWrite};
-
-#[cfg(feature = "std")]
-pub(crate) use futures_lite::future::block_on;
-
-#[cfg(not(feature = "std"))]
-pub(crate) use embassy_futures::block_on;
-
 pub(crate) use common::{
-    decode_var_int, encode_packet, packet_from, read_bytes, read_string, read_u16, read_u32,
-    read_u8, write_bytes, write_string, write_u16, write_u32, write_u8, write_var_int,
+    block_on, decode_var_int, encode_packet, packet_from, read_bytes, read_string, read_u16,
+    read_u32, read_u8, write_bytes, write_string, write_u16, write_u32, write_u8, write_var_int,
+    AsyncRead, AsyncWrite, SyncRead, SyncWrite,
 };
 
 pub use common::{
